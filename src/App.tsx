3945acfcba8354d2e19cd44293ba41ec15c9ee41
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-08-21 11:04:29 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import './App.css';
 import Survey from './survey/Survey';
 
 
-const App = () => {
+export const App = () => {
   return (
     <div className="App">
       <Survey />
     </div>
   );
 }
-
-export default App;
